37052719a3ffe9bd47bb03f89dec4fbc77e4711f
progresso85 <martin.heuripeau@gmail.com>
2022-09-21 14:29:06 +0200

    add UserPage -> my product
    Page my product and color for nav bar and link to add product

---
+++ dashboard-admin/src/views/UserPageView.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav class="product">
+    <a>my product</a>|
+    <router-link class="link" to="#">add product</router-link>
+  </nav>
+  
+  
+  </template>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+    html {
+      background: #fffffe;
+    }
+
+    nav {
+        background: #094067;
+
+    }
+
+    h1 {
+      color: #fffffe;
+    }
+
+    .link{
+        color: #fffffe;
+        text-align: center;
+    }
+
+    nav a {
+        color: #fffffe;
+    }
+
+    </style>
