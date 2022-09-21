7cf0acf8ee2794a611d1308d6a06a66e20b94cae
Antony <91181127+Getsugo@users.noreply.github.com>
2022-09-21 15:07:39 +0200

    nav bar

--- dashboard-admin/src/App.vue
+++ dashboard-admin/src/App.vue
@@ -10,34 +10,60 @@
   </script>
   
   <template>
-    <div id = 'bandeau'>
- 
-</div>
+    <nav>
     <h1>{{ message }}</h1>
-    <a href="https://google.com" >
+    <div class="right">
+    <a class="login" href="https://google.com" >
     login
   </a>
-  <button class="panierBtn"><img class="panier" src="./assets/panier.png" /></button>
+ <!-- <a href="#"><button class="panierBtn"></button></a>-->
+  <a  href="https://google.com"><img class="panierBtn" src="./assets/panier.png"  /></a></div>
+  </nav>
   </template>
 
 
 <style>
+  nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 80px;
+    position: fixed;
+    z-index: 210;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    background-color: #094067;
+    box-shadow: rgb(0 0 0 / 62%) 0 5px 20px;
+  }
+
+  .right {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
 
 
-  button, a {
+  h1 {
+    color: #ef4565;
+    font-size: 30px;
+    margin-left: 20px;
+  }
+
+
+  .login {
     display: block;
-    margin-bottom: 1em;
+    float: right;
+    color: white;
+    text-decoration : none;
+    margin-right:  40px;
   }
-  .panierBtn {
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 20%;
-    height: 20%;
 
+  .panierBtn {
+    width: 50px;
+    margin-right: 20px;
   }
-  .panier {
-    width: 20%;
-    height: 20%;
-  }
+  
+
+ 
   </style>
