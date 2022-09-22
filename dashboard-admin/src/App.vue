43fa41c0ea18f789601f2ca49372243fbc6cc09d
progresso85 <martin.heuripeau@gmail.com>
2022-09-22 09:24:40 +0200

    add component new product form

--- dashboard-admin/src/App.vue
+++ dashboard-admin/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/user">User</router-link>
+    </nav>
+  <router-view></router-view>
+</div>
 </template>
 
 <style>
@@ -23,6 +26,7 @@ nav a {
   font-weight: bold;
   color: #2c3e50;
 }
+
 
 nav a.router-link-exact-active {
   color: #42b983;
